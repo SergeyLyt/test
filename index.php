055a2647302d115a20c034f2de36e6dee167b679
SergeyLyt <87386293+SergeyLyt@users.noreply.github.com>
2021-07-28 23:29:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,78 +1,55 @@
 <?php
 
-$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
+/*function get_type($a){
 
-//var_dump(count($arr));//посчитать длину массива
+        echo gettype($a) . '<br>';
 
-//переместить первые 4 элемента массива в конец массива
-/*$count = 0;
-foreach ($arr as $k => $v){
-    $arr[] = array_shift($arr);
-   $count++;
-   if($count>=4){
-       break;
-   };
+    }
 
-}
-var_dump($arr)*/;
-
-//получить сумму 4,5,6 элемента
-//echo ($arr[3] + $arr[4] + $arr[5]);
+get_type(4); // integer
+get_type('Какой то текст'); // string
+get_type(true); // boolean
+get_type([1,2]); // array
+get_type(1.2); // double
+*/
 
 
 /*$firstArr = [
-    'one' => 1,
-    'two' => 2,
-    'three' => 3,
+    'one'   => 10,
+    'two'   => [17, 28, 32],
+    'three' => [10, 42],
     'foure' => 5,
-    'five' => 12,
-];
-$secondArr = [
-    'one' => 1,
-    'seven' => 22,
-    'three' => 32,
-    'foure' => 5,
-    'five' => 13,
-    'six' => 37,
-];*/
-//var_dump(array_diff($secondArr,$firstArr)); //отсутствуют в первом массиве и присутствуют во втором
+    'five'  => [ 32, 57, 72]];
 
-//var_dump(array_diff($firstArr,$secondArr)); //присутствую в первом и отсутствуют во втором
-
-//var_dump(array_intersect($firstArr,$secondArr)); //значения которых совпадают
-
-//var_dump(array_diff_assoc($firstArr,$secondArr)); //значения которых отличается
-
-$firstArr = [
-    'one'   => 1,
-    'two'   => ['one' => 1, 'seven' => 22, 'three' => 32,],
-    'three' => ['one' => 1, 'two' => 2,],
-    'foure' => 5,
-    'five'  => ['three' => 32, 'foure' => 5, 'five' => 12,]];
-
-//получить все вторые элементы вложенных массиво
-
-foreach ($firstArr as $k => $v){
-    if (is_array($v)){
-        var_dump(array_slice($v, 1, 1));
+function sumRecursive($firstArr){
+    $sum = 0;
+    foreach($firstArr as $v) {
+        if(is_array($v)){
+            $sum += sumRecursive($v);
+        } else{
+            $sum += $v;
+        }
     }
-
+    return $sum;
 }
+var_dump(sumRecursive($firstArr));*/
 
-//echo count ($firstArr, COUNT_RECURSIVE); //получить общее количество элементов в массиве
 
-/*function sumRecursive($firstArr){
-$sum = 0;
-foreach($firstArr as $v) {
-    if(is_array($v)){
-        $sum += sumRecursive($v);
-    } else{
-        $sum += $v;
+/*
+function search ($string, $char) {
+    foreach (count_chars($string, 1) as $i => $val) {
+        if ( chr($i)  == $char) {
+            echo $char. " встречается в строке $val раз(а).\n";
+        }
     }
 }
-        return $sum;
-}
-var_dump(sumRecursive($firstArr));*/ //получить сумму всех значений в массиве
+search('text have one b', 'b');*/
 
+/*$square_main = ['h' => 10, 'w' => 20];
+$square_litle = ['h=> 5, 'w' => 5];
+ function square ($main, $litle) {
+ returne ($main['h']*$main['w'])/($litle['h']*$litle['w'])
+}
+echo square($square_main, $square_litle);*/
 
 
